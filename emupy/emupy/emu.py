6cb45c81cb411fe57b71b648cf2498af5b0826ea
vrastil <michalvrastil@seznam.cz>
2017-11-22 16:36:03 -0600

    corr func

--- emupy/emupy/emu.py
+++ emupy/emupy/emu.py
@@ -54,4 +54,49 @@ class CosmoParam(object):
 
         if self.omega_nu < 0 or self.omega_nu > 0.01:
             raise ValueError("Omega_nu out of emulator range.")
-        
+
+
+import power_spec as pwr
+import numpy as np
+from scipy.optimize import curve_fit
+
+class Interp_obj(object):
+    def __init__(self, k, Pk):
+        self.interpolate = pwr.interpolate(k, Pk)
+
+class Extrap_Pk(Interp_obj):
+    def __init__(self, k, Pk):
+        Interp_obj.__init__(self, k, Pk)
+
+        def log_pk(log_k, n_s, log_A):
+            return log_A + n_s*log_k
+
+        self.par = np.empty(6)
+
+        popt = curve_fit(log_pk, np.log(k[0:10]), np.log(Pk[0:10]))[0]  # fit over a ten points
+        self.par[0] = k[0]
+        self.par[1] = popt[0]
+        self.par[2] = np.exp(popt[1])
+    
+        popt = curve_fit(log_pk, np.log(k[-10:]), np.log(Pk[-10:]))[0]  # fit over a ten points
+        self.par[3] = k[-1]
+        self.par[4] = popt[0]
+        self.par[5] = np.exp(popt[1])
+
+    def __call__(self, k):
+        return pwr.power_spectrum(k, self.par, self.interpolate)
+
+
+def correlation_function(Extrap_Pk, r_min=1, r_max=200, num=100):
+    r = np.linspace(r_min, r_max, num=num)
+    return r, pwr.corr_func(r, Extrap_Pk.par, Extrap_Pk.interpolate)
+
+def corr_func_tot(cosmo, z, r_min=1, r_max=200, num=100):
+    pk_tot, k = pwr.init_power_spectrum_tot(cosmo, z)
+    ext_tot = Extrap_Pk(k, pk_tot)
+    return correlation_function(ext_tot, r_min=r_min, r_max=r_max, num=num)
+
+def corr_func_cb(cosmo, z, r_min=1, r_max=200, num=100):
+    pk_cb, k = pwr.init_power_spectrum_cb(cosmo, z)
+    ext_cb = Extrap_Pk(k, pk_cb)
+    return correlation_function(ext_cb, r_min=r_min, r_max=r_max, num=num)
--- emupy/emupy/power_spec.pyx
+++ emupy/emupy/power_spec.pyx
@@ -1,5 +1,9 @@
 import numpy as np
 cimport numpy as np
+from numpy cimport ndarray
+
+from scipy.interpolate import PchipInterpolator
+from scipy.integrate import quad
 
 cdef extern void emu_tot(double *xstar, double *ystar, double* mode)
 cdef extern void emu_cb(double *xstar, double *ystar, double* mode)
@@ -12,8 +16,8 @@ def init_power_spectrum_tot(cosmo, z):
     cdef double xstar[9] 
     xstar[:] = [cosmo.omega_m, cosmo.omega_b, cosmo.sigma_8, cosmo.h,
                 cosmo.n_s, cosmo.w_0, cosmo.w_a, cosmo.omega_nu, z]
-    cdef np.ndarray[double, ndim=1, mode="c"] ystar = np.empty(351)
-    cdef np.ndarray[double, ndim=1, mode="c"] mode = np.empty(351)
+    cdef ndarray[double, ndim=1, mode="c"] ystar = np.empty(351)
+    cdef ndarray[double, ndim=1, mode="c"] mode = np.empty(351)
 
     # fill ystar (=Pk [Mpc^3]) and mode (=k [1/Mpc])
     emu_tot(xstar, <double*> ystar.data, <double*> mode.data)
@@ -28,13 +32,40 @@ def init_power_spectrum_cb(cosmo, z):
     cdef double xstar[9] 
     xstar[:] = [cosmo.omega_m, cosmo.omega_b, cosmo.sigma_8, cosmo.h,
                 cosmo.n_s, cosmo.w_0, cosmo.w_a, cosmo.omega_nu, z]
-    cdef np.ndarray[double, ndim=1, mode="c"] ystar = np.empty(351)
-    cdef np.ndarray[double, ndim=1, mode="c"] mode = np.empty(351)
+    cdef ndarray[double, ndim=1, mode="c"] ystar = np.empty(351)
+    cdef ndarray[double, ndim=1, mode="c"] mode = np.empty(351)
 
     # fill ystar (=Pk [Mpc^3]) and mode (=k [1/Mpc])
     emu_cb(xstar, <double*> ystar.data, <double*> mode.data)
 
     return ystar, mode
+
+cpdef interpolate(ndarray[double, ndim=1, mode="c"] k, ndarray[double, ndim=1, mode="c"] Pk):
+    return PchipInterpolator(k, Pk)
+
+cdef inline double power_law_pk(double k, double n_s, double A) except*:
+    return A*pow(k, n_s)
+
+cpdef power_spectrum(double k, ndarray[double, ndim=1, mode="c"] par, interpolate):
+    if k < par[0]:
+        return power_law_pk(k, par[1], par[2])
+    elif k < par[3]:
+        return interpolate(k)
+    else:
+        return power_law_pk(k, par[4], par[5])
+
+cdef double xi_integrand(double k, double r, ndarray[double, ndim=1, mode="c"] par, interpolate):
+    return 1. / (2. * np.pi**2) * k / r * power_spectrum(k, par, interpolate)
+
+cpdef corr_func(ndarray[double, ndim=1, mode="c"] r_vec, ndarray[double, ndim=1, mode="c"] par, interpolate):
+    cdef ndarray[double, ndim=1, mode="c"] xi = np.empty(r_vec.size)
+    cdef int i
+    cdef double r
+    for i in range(r_vec.size):
+        r = r_vec[i]
+        xi[i] = quad(xi_integrand, 0, np.inf, args=(r, par, interpolate), weight='sin', wvar=r)[0]
+    return xi
+
 
 # to get rid of ImportError: dynamic module does not define init function
 if __name__ == "__main__":
